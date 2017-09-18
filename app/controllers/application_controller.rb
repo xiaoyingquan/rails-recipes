8361aa9cb59e053a91413d5537b79f627bf08e6a
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-09-19 07:31:17 +0800

    switch the time zone according to user configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_action :set_timezone
 
   def set_locale
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
@@ -9,4 +10,11 @@ class ApplicationController < ActionController::Base
 
     I18n.locale = session[:locale] || I18n.default_locale
   end
+
+  def set_timezone
+    if current_user && current_user.time_zone
+      Time.zone = current_user.time_zone
+    end
+  end
+
 end
