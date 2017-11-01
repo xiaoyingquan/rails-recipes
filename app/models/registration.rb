d264eb59be43c90d65b6d52a6948c64949980926
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-11-02 06:33:16 +0800

    customizes data validation error display

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,6 +3,7 @@ class Registration < ApplicationRecord
   STATUS = ["pending", "confirmed"]
   validates_inclusion_of :status, :in => STATUS
   validates_presence_of :status, :ticket_id
+  validate :check_event_status, :on => :create
 
   attr_accessor :current_step
   validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
@@ -30,6 +31,12 @@ class Registration < ApplicationRecord
 
   def should_validate_all_data?
     current_step == 3 || status == "confirmed"  # 做到第三步，或是最后状态是 confirmed 时需要验证
+  end
+
+  def check_event_status
+    if self.event.status == "draft"
+      errors.add(:base, "活动尚未开放报名")
+    end
   end
 
 end
