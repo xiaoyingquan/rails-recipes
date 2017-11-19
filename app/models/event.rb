0d9875f5cd81efd40022a88122a44621ee481faf
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-11-20 07:02:56 +0800

    Upload multiple files (using additional Model)

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,6 +22,9 @@ class Event < ApplicationRecord
    has_many :tickets, :dependent => :destroy, :inverse_of => :event
    accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
+   has_many :attachments, :class_name => "EventAttachment", :dependent => :destroy
+   accepts_nested_attributes_for :attachments, :allow_destroy => true, :reject_if => :all_blank
+
    STATUS = ["draft", "public", "private"]
    validates_inclusion_of :status, :in => STATUS
 
