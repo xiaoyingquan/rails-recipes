1143e585f44d48e02a140f66126754d6783a4e56
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-11-22 06:12:19 +0800

    Analysis of the total amount of votes

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -26,7 +26,17 @@ class Admin::EventsController < AdminController
         borderWidth: 1
         }]
     }
-    
+
+    @data2 = {
+        labels: ticket_names,
+        datasets: [{
+            label: '# of Amount',
+            data:  @event.tickets.map{ |t| t.registrations.by_status("confirmed").count * t.price },
+            backgroundColor: colors,
+            borderWidth: 1
+          }]
+    }
+
   end
 
   def new
